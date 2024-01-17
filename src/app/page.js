ec5071bc82b56445c870209fc637ba5da7690ea5
Truong Quoc Khanh <workhanh79@gmail.com>
2024-01-17 09:39:34 +0700

    Modify navbar

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 import Header from '@/components/header'
-import Navbar from '@/components/navbar'
+import Navbar from '@/components/navbar/navbar'
 
 export default function Home() {
 	return (
